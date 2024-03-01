10e0bbbb3b9c5463fd6f281d5587048314ca9ad9
sahil-ansari01 <opensourcecarrier@gmail.com>
2024-03-01 07:51:57 +0530

    handled different error

--- public/js/signup.js
+++ public/js/signup.js
@@ -21,7 +21,20 @@ async function signup(e) {
             throw new Error('Failed to login')
         }
 
-    } catch (err) {
-        alert(err);
+    } catch (error) {
+        if (error.response) {
+            // If the error is a response from the server (e.g., status code 400)
+            if (error.response.status === 400) {
+                alert(error.response.data.error); 
+            } else {
+                alert('An error occurred'); 
+            }
+        } else if (error.request) {
+            // If the request was made but no response was received
+            alert('No response from server');
+        } else {
+            // If an error occurred during the request setup
+            alert('Error setting up request');
+        }
     }
 }
