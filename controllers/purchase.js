1ab3577a3e54ae1cb8b7d0e56be0c9141c9c6a87
sahil-ansari01 <opensourcecarrier@gmail.com>
2024-05-13 17:59:00 +0530

    razorpay integration final

--- controllers/purchase.js
+++ controllers/purchase.js
@@ -42,16 +42,13 @@ const updateTransactionStatus = async (req, res) => {
             return res.status(404).json({ message: 'Order not found' });
         }
 
-        // Check if payment_id exists. If it does, the transaction is successful.
-        if (payment_id) {
-            await order.update({ paymentid: payment_id, status: 'SUCCESSFUL' });
-            await req.user.update({ ispremiumuser: true });
+        const promise1 = order.update({ paymentid: payment_id, status: 'SUCCESSFUL' });
+        const promise2 = req.user.update({ ispremiumuser: true });
+        Promise.all([promise1, promise2]).then(() => {
             return res.status(202).json({ success: true, message: 'Transaction Successful!' });
-        } else {
-            // If payment_id is not present, mark the transaction as FAILED.
-            await order.update({ status: 'FAILED' });
-            return res.status(400).json({ success: false, message: 'Transaction Failed hoga hai bot!' });
-        }
+        }).catch(err => {
+            throw new Error(err);
+        })
         
     } catch(err) {
         console.error(err);
