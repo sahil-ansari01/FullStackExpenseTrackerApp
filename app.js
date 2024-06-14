81353f18f5b7c065466082f8e8c6ccff364cf107
sahil-ansari01 <opensourcecarrier@gmail.com>
2024-06-14 20:30:54 +0530

    helmet removed

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ const User = require("./models/user");
 const Order = require('./models/orders');
 const ForgetPasswordRequest = require('./models/forgetPasswordRequest');
 const Downloads = require('./models/downloads');
-const helmet = require('helmet');
-const morgan = require('morgan');
 
 const cors = require('cors');
 const app = express();
@@ -23,22 +21,7 @@ const purchaseRoutes = require('./routes/purchase');
 const premiumRoutes = require('./routes/premium');
 const passwordRoutes = require('./routes/password');
 
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), 
-    {flags: 'a'}
-);
-
 app.use(cors());
-
-app.use(helmet());
-app.use((req, res, next) => {
-    res.setHeader(
-        "Content-Security-Policy",
-        "script-src 'self' cdnjs.cloudflare.com cdn.jsdelivr.net checkout.razorpay.com 'unsafe-inline'"
-    );
-    next();
-});
-
-app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
